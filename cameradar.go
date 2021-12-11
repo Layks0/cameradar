1f43d1f1db5d7d853adecb28e86fd79cbb168680
Layks0 <88584417+Layks0@users.noreply.github.com>
2021-12-11 13:15:46 +0200

    Update cameradar.go

--- cameradar.go
+++ cameradar.go
@@ -1,14 +1,9 @@
-// Package cameradar provides methods to be able to discover and
-// attack RTSP streams easily. RTSP streams are used by most
-// IP Cameras, often for surveillance.
-//
-// A simple example usage of the library can be found in
-// https://github.com/Ullaakut/cameradar/tree/master/cameradar
-//
-// The example usage is complete enough for most users to
-// ignore the library, but for users with specific needs
-// such as creating their own bruteforcing dictionary to
-// access cameras, or running their own network scan, this
-// library allows to use simple and performant methods to
-// attack streams.
-package cameradar
+sudo apt-get update && apt-get upgrade
+sudo apt-get install golang
+sudo apt-get install libcur14-openss1-dev pkg
+go get github.com/Ullaakut/cameradar
+cd $GOPATH/src/github.com/Ullaakut/cameradar
+cd cmd/cameradar
+go install
+cameradar -t <109.237.2.66>
+
